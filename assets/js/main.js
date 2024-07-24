7927ea55b482d23b4bf051d67305d68ba71a6d77
Chhenlee <chhenly.personal@gmail.com>
2024-07-24 20:15:50 +0700

    change title-section

--- assets/js/main.js
+++ assets/js/main.js
@@ -148,7 +148,7 @@ ScrollReveal({
 });
 
 // Target elements, and specify options to create reveal animations
-ScrollReveal().reveal('.home .info h2, .section-title-01, .section-title-02', { delay: 500, origin: 'left' });
+ScrollReveal().reveal('.home .info h2, .section-title', { delay: 500, origin: 'left' });
 ScrollReveal().reveal('.home .info h3, .home .info p, .about-info .btn', { delay: 600, origin: 'right' });
 ScrollReveal().reveal('.home .info .btn, .home .scroll-down', { delay: 700, origin: 'bottom' });
 ScrollReveal().reveal('.media-icons i, .contact-left li', { delay: 500, origin: 'left', interval: 200 });
